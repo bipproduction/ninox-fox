ad5f7dc865fdf85eebc9bc525e8f864b86fe66e7
amel <amel@amel.local>
2024-07-12 16:38:03 +0800

    upd: tambahan
    Deskripsi:
    - update seeder component dan user akses
    - fitur setting data emosi kandidat
    - update fitur setting data emosi kandidat
    
    No Issues

---
+++ src/app/dashboard/(developer)/setting-data/page.tsx
@@ -0,0 +1,35 @@
+import { MasterProvinceGetAll, MasterKabGetByProvince } from "@/modules/_global";
+import { funGetCandidateActiveByArea, funGetUserDefaultFront } from "@/modules/candidate";
+import { ViewSettingEmotion } from "@/modules/emotion";
+import _ from "lodash";
+import moment from "moment";
+
+export default async function Page({ searchParams }: { searchParams: { prov: any, city: any, can: any, date: any } }) {
+   const defaultValue = await funGetUserDefaultFront()
+   const kabVal = (_.isNull(defaultValue.idKabkot) || _.isUndefined(defaultValue.idKabkot)) ? 0 : defaultValue.idKabkot
+   const today = moment(new Date()).format('YYYY-MM-DD')
+
+   // const findData = {
+   //    idProvinsi: (_.isNaN(Number(searchParams.prov)) ? 0 : Number(searchParams.prov)),
+   //    idKabkot: (_.isNaN(Number(searchParams.city)) ? 0 : Number(searchParams.city)),
+   //    tingkat: (_.isNaN(Number(searchParams.city)) ? 1 : 2),
+   //    idCandidate: (_.isUndefined(searchParams.can) ? 0 : searchParams.can),
+   //    date: (_.isUndefined(searchParams.date) ? null : searchParams.date)
+   // }
+
+   const findData = {
+      idProvinsi: (_.isNaN(Number(searchParams.prov)) ? defaultValue.idProvinsi : Number(searchParams.prov)),
+      idKabkot: (_.isNaN(Number(searchParams.city)) && _.isNaN(Number(searchParams.prov)) ? kabVal : _.isNaN(Number(searchParams.city)) ? 0 : Number(searchParams.city)),
+      tingkat: (_.isNaN(Number(searchParams.city)) && _.isNaN(Number(searchParams.prov)) ? defaultValue.tingkat : _.isNaN(Number(searchParams.city)) ? 1 : 2),
+      idCandidate: (_.isUndefined(searchParams.can) ? defaultValue.idCandidate : searchParams.can),
+      date: (_.isUndefined(searchParams.date) ? today : searchParams.date)
+   }
+
+   const prov = await MasterProvinceGetAll()
+   const city = await MasterKabGetByProvince({ idProvinsi: findData.idProvinsi })
+   const candidate = await funGetCandidateActiveByArea({ find: findData })
+
+   return (
+      <ViewSettingEmotion param={findData} provinsi={prov} kabupaten={city} candidate={candidate} />
+   )
+}
