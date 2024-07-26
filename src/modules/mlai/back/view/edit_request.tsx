ad7b9b2478069b7538ce2eb251014a0c259d190c
amel <amel@amel.local>
2024-07-26 10:49:23 +0800

    upd: tambahan edit request

--- src/modules/mlai/back/view/edit_request.tsx
+++ src/modules/mlai/back/view/edit_request.tsx
@@ -2,20 +2,35 @@
 import { ButtonBack } from "@/modules/_global";
 import { Stack, Box, Spoiler, Group, TextInput, Select, Button, Modal, Text, ActionIcon, Textarea } from "@mantine/core";
 import { DateInput, TimeInput } from "@mantine/dates";
-import { RichTextEditor } from "@mantine/tiptap";
-import router from "next/router";
-import { CiPickerEmpty } from "react-icons/ci";
-import ModalAddMlAi from "../component/modal_add_mlai";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { AiOutlineClockCircle } from "react-icons/ai";
+import moment from "moment";
+import toast from "react-simple-toasts";
+import ModalEditRequest from "../component/modal_edit_request";
+import { useAtom } from "jotai";
+import { isModalMlAi } from "../val/val_mlai";
 
 export default function EditRequest({ data }: { data: any }) {
    const ref = useRef<HTMLInputElement>(null);
+   const [openModal, setOpenModal] = useAtom(isModalMlAi)
+   const [isBody, setBody] = useState({
+      id: data.id,
+      request: data.request,
+      date: data.date == null ? '' : data.date,
+      time: data.time == null ? '' : data.time
+   })
+
    const pickerControl = (
       <ActionIcon variant="subtle" color="gray" onClick={() => ref.current?.showPicker()}>
          <AiOutlineClockCircle style={{ width: "70%", height: "70%" }} />
       </ActionIcon>
    );
+
+   function onConfirmation() {
+      if (Object.values(isBody).includes("") || Object.values(isBody).includes(null))
+         return toast("Form cannot be empty", { theme: "dark" });
+      setOpenModal(true)
+   }
 
    return (
       <>
@@ -28,25 +43,25 @@ export default function EditRequest({ data }: { data: any }) {
                <DateInput valueFormat="DD-MM-YYYY"
                   label={"Tanggal"}
                   placeholder="Pilih Tanggal"
-               // value={(isDataMlai.dateContent == '') ? null : new Date(isDataMlai.dateContent)}
-               // onChange={(e) => {
-               //    setDataMlai({
-               //       ...isDataMlai,
-               //       dateContent: moment(e).format("YYYY-MM-DD"),
-               //    });
-               // }}
+                  value={(isBody.date == '') ? null : new Date(isBody.date)}
+                  onChange={(e) => {
+                     setBody({
+                        ...isBody,
+                        date: moment(e).format("YYYY-MM-DD"),
+                     });
+                  }}
                />
                <TimeInput
                   label="Jam"
                   ref={ref}
                   rightSection={pickerControl}
-               // value={isDataMlai.timeContent}
-               // onChange={(val) =>
-               //    setDataMlai({
-               //       ...isDataMlai,
-               //       timeContent: String(val.target.value)
-               //    })
-               // }
+                  value={isBody.time}
+                  onChange={(val) =>
+                     setBody({
+                        ...isBody,
+                        time: String(val.target.value)
+                     })
+                  }
                />
             </Group>
             <Box pt={30}>
@@ -54,40 +69,32 @@ export default function EditRequest({ data }: { data: any }) {
                   size="md"
                   radius="md"
                   label="Request"
+                  value={isBody.request}
+                  onChange={(val) => {
+                     setBody({
+                        ...isBody,
+                        request: val.target.value
+                     })
+                  }}
                />
             </Box>
             <Group justify="flex-end">
                <Button bg={"gray"} mt={30} size="md"
-                  // onClick={onConfirmation}
+                  onClick={onConfirmation}
                >
                   SIMPAN
                </Button>
             </Group>
          </Box>
-         {/* <Modal
+         <Modal
             opened={openModal}
             onClose={() => setOpenModal(false)}
             centered
             withCloseButton={false}
             closeOnClickOutside={false}
          >
-            <ModalAddMlAi text={editor?.getHTML()} data={isDataMlai}
-               onSuccess={() => {
-                  if (req.id != null)
-                     return router.push('/dashboard/ml-ai/request')
-                  editor?.commands.setContent('<p></p>')
-                  // setProvinsi(null)
-                  // setKabupaten(null)
-                  setDataMlai({
-                     ...isDataMlai,
-                     idRequest: null,
-                     idCandidate: '',
-                     dateContent: '',
-                     timeContent: '',
-                  })
-               }}
-            />
-         </Modal> */}
+            <ModalEditRequest data={isBody}/>
+         </Modal>
       </>
    )
 }
